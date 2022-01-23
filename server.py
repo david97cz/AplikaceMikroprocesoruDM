f4b390bdcbdcbb95677bbb69840475b115b670b9
david97cz <91188542+david97cz@users.noreply.github.com>
2022-01-23 16:56:11 +0100

    Add files via upload

--- server.py
+++ server.py
@@ -2,10 +2,23 @@ import asyncio
 import datetime
 import random
 import websockets
+import psutil
+
+
+
+
 
 async def show_time(websocket):
     while websocket.open:
-        await websocket.send("teplota je 30 C a vlhkost 25%")
+        temp = psutil.sensors_temperatures()
+        temp2 = temp.get("cpu_thermal")
+        tempstr = str(temp2[0])
+        str_pos = tempstr.find("current")
+        temp_pos = str_pos+len("current=")
+        tempCPU=(tempstr[temp_pos]+tempstr[temp_pos+1]+","+tempstr[temp_pos+3]+tempstr[temp_pos+4])
+        print(tempCPU)
+        
+        await websocket.send(tempCPU)
         await asyncio.sleep(2)
 
 async def main():
